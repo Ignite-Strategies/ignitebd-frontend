71ae09305034af35669ed49cadd2927fbf26ec80
adamgofast <adam@gofastcrushgoals.com>
2025-11-03 14:08:27 -0500

    Fix ActivationContext import paths in assessment folder

--- src/pages/assessment/Cost.jsx
+++ src/pages/assessment/Cost.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useActivation } from '../context/ActivationContext';
+import { useActivation } from '../../context/ActivationContext';
 import api from '../../lib/api';
 
 export default function Cost() {
--- src/pages/assessment/Human.jsx
+++ src/pages/assessment/Human.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useActivation } from '../context/ActivationContext';
+import { useActivation } from '../../context/ActivationContext';
 
 export default function Human() {
   const navigate = useNavigate();
